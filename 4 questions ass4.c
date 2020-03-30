11754343b556d9d8a381d639a1c045519954b69f
Nalini1526 <61822037+Nalini1526@users.noreply.github.com>
2020-03-30 01:55:55 -0500

    Add files via upload

---
+++ 4 questions ass4.c
@@ -0,0 +1,44 @@
+/*4. Write a c program for Binary Search Algorithm*/
+
+
+#include<stdio.h>
+ 
+int main()
+{
+    int arr[10],i,n,x,flag=0,first,last,mid;
+ 
+    printf("Enter size of array:");
+    scanf("%d",&n);
+    printf("\nEnter array element(ascending order)\n");
+ 
+    for(i=0;i<n;++i)
+        scanf("%d",&arr[i]);
+ 
+    printf("\nEnter the element to search:");
+    scanf("%d",&x);
+ 
+    first=0;
+    last=n-1;
+ 
+    while(first<=last)
+    {
+        mid=(first+last)/2;
+ 
+        if(x==arr[mid]){
+            flag=1;
+            break;
+        }
+        else
+            if(x>arr[mid])
+                first=mid+1;
+            else
+                last=mid-1;
+    }
+ 
+    if(flag==1)
+        printf("\nElement found at position %d",mid+1);
+    else
+        printf("\nElement not found");
+ 
+    return 0;
+}
